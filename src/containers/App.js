f245b8a432086669c27d6f3c2b5fbc7f1f39e2d9
akii <akii.jadhav22@gmail.com>
2021-01-13 00:27:12 +0530

    refactoring code to use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,45 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 // import {robots} from './Robots';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searches: '',
-        }
+function App () {
+    const [robots, setRobots] = useState([])
+    const [searches, setSearches] = useState('')
+    // const [count, setCount] = useState(0)
+
+    useEffect(() => {
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(response => response.json())
+                .then(users => {setRobots( users )})
+                // console.log(count)
+    }, []) // only run if count changes.
+    //empty dependency means componentDidMount i.e run once & no dependency means loop i.e run forever
+    //& with a dependency it means run the useEffect agn when the dependency i.e the passed in dep - state changes
+
+   const onSearchChange = (event) => {
+        setSearches(event.target.value)
     }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => {
-                this.setState({robots: users})
-            })
-    }
+    const filteredRobot = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searches.toLowerCase());
+    })
 
-    onSearchChange = (event) => {
-        this.setState({searches: event.target.value})
-    }
+    return !robots.length ?
+        <h1 className="tc">LOADING</h1> :
+        (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                {/*<button onClick={() => setCount(count + 1)}>Click Me!</button>*/}
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobot}/>
+                </Scroll>
+            </div>
+        )
 
-    render() {
-        const {robots, searches} = this.state;
-        const filteredRobot = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searches.toLowerCase());
-        })
-        return !robots.length ?
-            <h1 className="tc">LOADING</h1> :
-            (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <CardList robots={filteredRobot}/>
-                    </Scroll>
-                </div>
-            )
     }
-}
 
 export default App;
